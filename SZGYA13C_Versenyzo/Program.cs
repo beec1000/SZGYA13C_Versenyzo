d562c305e4aad70909c87002d8404b7a1a1f8b69
beec1000 <beec1000.minecraft@gmail.com>
2024-10-09 12:16:21 +0200

    Add project files.

---
+++ SZGYA13C_Versenyzo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SZGYA13C_Versenyzo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ SZGYA13C_Versenyzo/Versenyzo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SZGYA13C_Versenyzo
+{
+    internal class Versenyzo
+    {
+        public string Nev { get; set; }
+        public List<int> Pontok { get; set; }
+
+        public Versenyzo(string nev, List<int> pontok)
+        { 
+            Nev = nev;
+            Pontok = pontok;
+        }
+
+        public static List<Versenyzo> FromFile(string path)
+        {
+            List<Versenyzo> versenyzok = new List<Versenyzo>();
+
+            string[] line = File.ReadAllLines(path);
+
+            foreach (var l in line)
+            {
+
+            }
+        }
+    }
+}
